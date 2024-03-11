b7ab421949563a502753fab060e822f6340f268b
hewelbelmonte <130703340+hewelbelmonte@users.noreply.github.com>
2024-03-11 00:36:17 -0300

    subindo

--- js/main.js
+++ js/main.js
@@ -1,60 +1,68 @@
+let cadastros = [];
+
 function criarFormulario() {
-    document.getElementById("criarFormulario").style.display = "none";
-    document.getElementById("p-header").style.display = "none";
-
-    let formulario = document.createElement("form");
-    let tituloNome = document.createElement("h2");
-    tituloNome.textContent = "Cadastro";
-
-    let divNome = document.createElement("div");
-    divNome.style.display = "flex";
-    divNome.style.justifyContent = "center";
-    divNome.style.alignItems = "center";
-    
-    let labelNome = document.createElement("label");
-    labelNome.textContent = "Nome:";
-    let inputNome = document.createElement("input");
-    inputNome.setAttribute("type", "text");
-    inputNome.setAttribute("name", "nome");
-    divNome.appendChild(labelNome);
-    divNome.appendChild(inputNome);
-
-    let divEmail = document.createElement("div");
-    divEmail.style.display = "flex";
-    divEmail.style.justifyContent = "center";
-    divEmail.style.alignItems = "center";
-    
-    let labelEmail = document.createElement("label");
-    labelEmail.textContent = "Email:";
-    let inputEmail = document.createElement("input");
-    inputEmail.setAttribute("type", "email");
-    inputEmail.setAttribute("name", "email");
-    divEmail.appendChild(labelEmail);
-    divEmail.appendChild(inputEmail);
-
-    let divFilme = document.createElement("div");
-    divFilme.style.display = "flex";
-    divFilme.style.justifyContent = "center";
-    divFilme.style.alignItems = "center";
-    
-    let labelFilme = document.createElement("label");
-    labelFilme.textContent = "Filme Favorito:";
-    let inputFilme = document.createElement("input");
-    inputFilme.setAttribute("type", "text");
-    inputFilme.setAttribute("name", "filme");
-    divFilme.appendChild(labelFilme);
-    divFilme.appendChild(inputFilme);
-
-    let btnEnviar = document.createElement("button");
-    btnEnviar.setAttribute("type", "submit");
-    btnEnviar.textContent = "CRIAR";
-    formulario.appendChild(divNome);
-    formulario.appendChild(divEmail);
-    formulario.appendChild(divFilme);
-    formulario.appendChild(btnEnviar);
-
     let container = document.getElementById("formulario-container");
     container.innerHTML = "";
+    container.style.display = "block";
+
+    let formulario = document.createElement("form");
+    formulario.id = "formulario-sorteio"; 
+    let tituloNome = document.createElement("h2");
+    tituloNome.id = "cadastro-form";
+    tituloNome.textContent = "Preencha e participe do sorteio ↓";
+
+    formulario.appendChild(criarDivComInput("Nome: ", "text", "nome"));
+    formulario.appendChild(criarDivComInput("Email: ", "email", "email"));
+    formulario.appendChild(criarDivComInput("Filme Favorito:", "text", "filme"));
+
+    let btnEnviar = document.createElement("button");
+    btnEnviar.setAttribute("type", "button");
+    btnEnviar.textContent = "PARTICIPAR";
+    btnEnviar.id = "participar-button"
+    formulario.appendChild(btnEnviar);
+
     container.appendChild(tituloNome);
     container.appendChild(formulario);
+
+   
+    btnEnviar.onclick = function() {
+        if (validarFormulario()) {
+            let nome = document.querySelector('input[name="nome"]').value;
+            let email = document.querySelector('input[name="email"]').value;
+            let filme = document.querySelector('input[name="filme"]').value;
+
+            let novoCadastro = { nome, email, filme };
+            cadastros.push(novoCadastro);
+
+            mostrarRespostas();
+        } else {
+            console.log("Por favor, preencha todos os campos requeridos.");
+        }
+    };
+}
+
+function validarFormulario() {
+    let formulario = document.getElementById("formulario-sorteio");
+    return formulario.checkValidity();
+}
+
+function criarDivComInput(labelText, inputType, inputName) {
+    let div = document.createElement("div");
+    div.style.display = "flex";
+    div.style.justifyContent = "center";
+    div.style.alignItems = "center";
+
+    let label = document.createElement("label");
+    label.textContent = labelText;
+    let input = document.createElement("input");
+    input.setAttribute("type", inputType);
+    input.setAttribute("name", inputName);
+
+    
+    if (inputName === "nome") {
+        input.setAttribute("placeholder", "Digite seu nome (ex: Andrei)");
+        input.setAttribute("required", true);
+        input.setAttribute("pattern", "[A-Za-zÀ-ú]{6,}");
+        input.setAttribute("title", "Nome deve ter no mínimo 6 letras");
+    }
 }
